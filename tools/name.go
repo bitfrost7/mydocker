cc6de86b60cbe68b1d34e54a7dee645dc884ec29
邹海强 <nuejiu629046@163.com>
2024-03-12 11:42:37 +0800

    1

--- tools/name.go
+++ tools/name.go
@@ -3,14 +3,12 @@ package tools
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
 var adjectives = []string{"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy"}
 var nouns = []string{"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake"}
 
 func GenerateDefaultName() string {
-	rand.Seed(time.Now().UnixNano())
 	adj := adjectives[rand.Intn(len(adjectives))]
 	noun := nouns[rand.Intn(len(nouns))]
 	return fmt.Sprintf("%s_%s", adj, noun)
